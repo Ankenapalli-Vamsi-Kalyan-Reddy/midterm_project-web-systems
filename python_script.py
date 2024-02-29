e3f7bfb99ecca0018cb3b97549e40d8a91622833
Ankenapalli-Vamsi-Kalyan-Reddy <avamsikalyanreddy@gmail.com>
2024-02-28 23:23:21 -0500

    Added functionality to track the number of times an order has been placed

--- python_script.py
+++ python_script.py
@@ -4,17 +4,34 @@ with open(r'C:\Users\customer\Desktop\IS601\midterm_project\example_orders.json'
     data = json.load(f)
 
 dictionary1 = {}
-dictionary2 = {}
+
+#dictionary2 = {}
 
 
 for i in data:
     dictionary1[i['phone']] = i['name']
-    for j in i['items']:
-        dictionary2[j['name']] = j['price']
+    #for j in i['items']:
+        #dictionary2[j['name']] = j['price']
 print(dictionary1)
-print(dictionary2)
+
+output_dict = {}
+
+# Iterate through the data and extract information about each item
+for order in data:
+    for item in order['items']:
+        item_name = item['name'].lower()
+        if item_name not in output_dict:
+            output_dict[item_name] = {
+                'price': item['price'],
+                'orders': 1  # Initialize the count to 1 for the first occurrence
+            }
+        else:
+            output_dict[item_name]['orders'] += 1  # Increment the count for subsequent occurrences
+
+# Print the output dictionary
+print(output_dict)
 with open('customers.json', 'w') as f:
     json.dump(dictionary1, f, indent=4)
 
 with open('items.json', 'w') as f:
-    json.dump(dictionary2, f, indent=4)
+    json.dump(output_dict, f, indent=4)
